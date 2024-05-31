0fcdcec9261a4b384a95485a7b5e1c41d8eda1e8
Ludovic Ollagnier <ludovic@tectec.fr>
2024-05-31 11:42:31 +0200

    End demo on UIHostingController

--- Projects/UISwiftKit/UISwiftKit/ViewController.swift
+++ Projects/UISwiftKit/UISwiftKit/ViewController.swift
@@ -39,10 +39,12 @@ class ViewController: UIViewController {
         let swiftUIView = AppIconView(iconName: "leaf")
         let someViewController = UIHostingController(rootView: swiftUIView)
 
-        // Embedding View Controller / Add child View Controller
-
         guard let extractedView = someViewController.view else { return }
         view.addSubview(extractedView)
+
+        // Embedding View Controller / Add child View Controller to allow communication
+        self.addChild(someViewController)
+        someViewController.didMove(toParent: self)
 
         extractedView.translatesAutoresizingMaskIntoConstraints = false
         extractedView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
@@ -50,6 +52,17 @@ class ViewController: UIViewController {
 
         extractedView.widthAnchor.constraint(equalToConstant: 100).isActive = true
         extractedView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+
+
+        // Using private API _UIHostingView /!\ Not app store safe
+        let view = _UIHostingView(rootView: swiftUIView)
+        self.view.addSubview(view)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        view.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: 150).isActive = true
+
+        view.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        view.heightAnchor.constraint(equalToConstant: 100).isActive = true
     }
 
     func showSwiftUIView() {
